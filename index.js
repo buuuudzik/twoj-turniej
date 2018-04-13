3bd5bdc7b4db267b3ceaef10fe03803af726c8c1
buuuudzik <slawomir.budzynski@gmail.com>
2018-04-13 18:11:52 +0200

    Add classes and fill Team properties

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 'use strict';
+
+let lang = 'en';
+
+const ERRORS = {
+    LACK_OF_NAME: {
+        en: 'You must type a name',
+        pl: 'Musisz podać imię'
+    }
+}
+
+
+class Team {
+    constructor(name) {
+        if (!name) throw Error(ERRORS.LACK_OF_NAME);
+        
+        this.name = name;
+        this.won = 0;
+        this.draw = 0;
+        this.lost = 0;
+        this.goalsScored = 0;
+        this.goalsLost = 0;
+    }
+}
+
+
+class Match {
+
+}
+
+
+class League {
+
+}
+
+
+class Tournament {
+
+}
+
+
+class App {
+
+}
