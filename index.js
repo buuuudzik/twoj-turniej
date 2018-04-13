5273a63d0deb25034e60d12ad3c8592e23c70679
buuuudzik <slawomir.budzynski@gmail.com>
2018-04-13 22:05:50 +0200

    Adding logic of first 2 configuration screens

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict';
 
 // USEFUL FUNCTIONS
-
-
+function hide(objJQ) {
+    if (!objJQ.hasClass('hidden')) objJQ.addClass('hidden');
+};
+function show(objJQ) {
+    if (objJQ.hasClass('hidden')) objJQ.removeClass('hidden');
+};
+function goToPage(objJQ) {
+    DOM.allPagesJQ.each(function() {hide($(this))});
+    show(objJQ);
+};
 
 // CHOOSE A LANGUAGE
 let lang = 'en';
@@ -34,11 +42,16 @@ const ALERTS = {
     LACK_OF_NAME: {
         en: 'You must type a name',
         pl: 'Musisz podać imię'
+    },
+    MAX_NUMBER_OF_TEAMS: {
+        en: 'You cannot add more than 64 teams!',
+        pl: 'Nie możesz dodać więcej niż 64 drużyny!'
     }
 }
 
 const DOM = {
     // PAGES
+    allPagesJQ: $('.page'),
     addingTeamsPageJQ: $('#adding-teams'),
     tourSettingsPageJQ: $('#tour-settings'),
     leagueViewPageJQ: $('#league-view'),
@@ -48,7 +61,17 @@ const DOM = {
     newTeamJQ: $('.team-name'),
     addTeamBtnJQ: $('.add-team'),
     addedTeamsJQ: $('.added-teams'),
-    readyTeamsBtnJS: $('.ready-teams'),
+    readyTeamsBtnJQ: $('.ready-teams'),
+    selectTourTypeJQ: $('#tour-type'),
+    selectLeagueJQ: $('.league-type'),
+    selectCupJQ: $('.cup-type'),
+    selectLeagueCupJQ: $('.league-cup-type'),
+    backToAddingTeamsJQ: $('.back-to-teams'),
+    startTourBtnJQ: $('.start-tour'),
+    leagueRevengeJQ: $('.league-revenge'),
+    leagueRevengeValJQ: $('#league-revenge-value'),
+    cupRevengeJQ: $('.cup-revenge'),
+    cupRevengeValJQ: $('#cup-revenge-value'),
 }
 
 // GLOBAL EMITTER OBJECTS FOR CONTROLLING OF DATA FLOW IN APP
@@ -76,6 +99,7 @@ class Team {
         this.won = 0;
         this.draw = 0;
         this.lost = 0;
+        this.points = 0; // HOW WITH POINTS WHEN STAGE IS CHANGING? CLEARING?
         this.goalsScored = 0;
         this.goalsLost = 0;
         this.matches = [];
@@ -140,14 +164,52 @@ class Tour {
 
         tourEmitter.listen('deletedTeam', (name) => {
             this.teams.splice(this.teams.findIndex(t => t.name === name), 1);
+            if (this.teams.length < 2) hide(DOM.readyTeamsBtnJQ);
         });
     }
     addTeam(newTeam) {
         if (!newTeam) return alert(ALERTS.LACK_OF_NAME[lang]);
-        this.teams.push(new Team(newTeam));        
+        if (this.teams.length >= 64) return alert(ALERTS.MAX_NUMBER_OF_TEAMS);
+        this.teams.push(new Team(newTeam));
+        if (this.teams.length >= 2) show(DOM.readyTeamsBtnJQ);
+        this.showOnlyPossibleTypes();   
     }
     removeTeam(team) {
 
+    }
+    canPlayCup() {
+        let numberOfTeams = this.teams.length;
+        if (numberOfTeams%2 !== 0 || ![2,4,8,16,32,64].includes(numberOfTeams)) return false;
+        else return true;
+    }
+    showOnlyPossibleTypes() {
+        let {selectCupJQ, cupRevengeJQ, cupRevengeValueJQ} = DOM;
+        if (this.canPlayCup()) {
+            show(selectCupJQ);
+        } else {
+            hide(selectCupJQ);
+            DOM.cupRevengeJQ[0].checked = false;
+            hide(cupRevengeJQ);
+        };
+        this.toggleRevengeChexboxes();
+    }
+    toggleRevengeChexboxes() {
+        let {selectTourTypeJQ, cupRevengeJQ, leagueRevengeJQ} = DOM;
+        let selectedType = selectTourTypeJQ.val();
+
+        if (selectedType === 'Liga') {
+            show(leagueRevengeJQ);
+            hide(cupRevengeJQ);
+            cupRevengeJQ[0].checked = false;
+        } else if (selectedType === 'Puchar') {
+            show(cupRevengeJQ);
+            hide(leagueRevengeJQ);
+            leagueRevengeJQ[0].checked = false;
+        } else if (selectedType === 'Liga+Puchar') {
+            show(leagueRevengeJQ);
+            if (this.canPlayCup()) show(cupRevengeJQ);
+            else hide(cupRevengeJQ);
+        };
     }
 }
 
@@ -156,6 +218,7 @@ const tour = new Tour();
 
 
 DOM.addTeamBtnJQ.on('click', function() {
+    // JEŚLI JUŻ JEST 64 DRUŻYNY, NIE MOŻNA DODAĆ WIĘCEJ NIŻ 64 DRUŻYNY
     let {newTeamJQ} = DOM;
     let newTeam = newTeamJQ.val();
     if (!newTeam) return alert(ALERTS.LACK_OF_NAME[lang]);
@@ -163,6 +226,14 @@ DOM.addTeamBtnJQ.on('click', function() {
     newTeamJQ.val('');
 });
 
-DOM.readyTeamsBtnJS.on('click', function() {
-    // WHAT WHEN TEAM LIST IS READY?
+DOM.readyTeamsBtnJQ.on('click', function() {
+    goToPage(DOM.tourSettingsPageJQ);
+});
+
+DOM.backToAddingTeamsJQ.on('click', function() {
+    goToPage(DOM.addingTeamsPageJQ);
+});
+
+DOM.selectTourTypeJQ.on('change', function() {
+    tour.toggleRevengeChexboxes();
 });
